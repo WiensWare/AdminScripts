5698a562dbf14d0b7fae91b9cb0b914a6895dec8
WiensWare <jeff@wiensware.com>
2018-07-03 11:50:52 -0700

    Created UserReport.ps1 script

--- README.md
+++ README.md
@@ -11,3 +11,4 @@ This is my collection of scripts and tools that I use on a monthly basis.  It is
 * ComputerReport.ps1 - Generates a report of computer objects from active directory.  Useful for inventory and deleting stale objects.
 * CreateHomeDirectory.ps1 - Creates a user's network-based home directory on a remote server.
 * patch-wim.ps1 - Updates a Windows 10 WIM file with patches downloaded from Microsoft.
+* UserReport.ps1 - Creates CSV of user account info.  Useful for auditing inactive accounts.
---
+++ UserReport.ps1
@@ -0,0 +1,52 @@
+﻿#Requires -module ActiveDirectory
+
+# Generates a CSV file containing all user accounts and related info
+# Useful to audit inactive accounts
+
+
+
+<#
+.Synopsis
+   Lists all users in an AD OU
+.DESCRIPTION
+   Searches the specified OU for all user names that match the provided filter
+.EXAMPLE
+   Get-ComputerFromOU -OU "OU=Users,OU=HR,DC=example,DC=net"
+.INPUTS
+   AD OU Path
+.OUTPUTS
+   String array of computer names   
+#>
+function Get-UserFromOU
+{
+    [CmdletBinding()]
+    [OutputType([string[]])]
+    Param
+    (
+        # Org Unit to get user from
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [string]
+        $OU,
+
+        # Filter of user names
+        [string]
+        $Filter="*"
+    )
+
+    Get-ADUser -SearchBase $OU -Filter {Name -like $Filter} -Properties *
+}
+
+
+
+
+# Get list of all computers in Server OU
+$Users = Get-UserFromOU -OU "OU=Users,OU=HR,DC=example,DC=net"
+
+# Export user account info to CSV file
+$Users | Select DisplayName, EmailAddress, SmartcardLogonRequired, LastLogonDate, PasswordLastSet, UserPrincipalName, Enabled, Description | sort LastLogonDate | Export-Csv -NoTypeInformation -Path "Users.csv"
+
+# Open the CSV file (usually in Excel)
+Invoke-Item "users.csv"
+
