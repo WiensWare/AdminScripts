e9f7280d5c912ec4603a9c15f52839951bda921b
WiensWare <jeff@wiensware.com>
2019-12-05 08:21:14 -0800

    Create remote-reboot.ps1

---
+++ Patching/remote-reboot.ps1
@@ -0,0 +1,12 @@
+﻿# Schedules a task on a remote computer to reboot at a specified time.
+
+$computername = Read-Host "Enter computer name to reboot" 
+[DateTime] $when = "11:55pm"
+
+Invoke-Command -ScriptBlock { param ( $when )
+    $trigger = New-ScheduledTaskTrigger -at $when -Once
+    $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "Restart-Computer -force"
+    $principal = New-ScheduledTaskPrincipal -RunLevel Highest -UserId "LOCALSERVICE" -LogonType ServiceAccount
+    $task = New-ScheduledTask -Action $action -Trigger $trigger -Principal $principal
+    $scheduled = Register-ScheduledTask -TaskName "Reboot Computer" -InputObject $task -Force
+} -ComputerName $computername -ArgumentList $when
